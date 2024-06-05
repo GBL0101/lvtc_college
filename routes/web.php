995b796d0c6752188d9288aa2205918331b9dedf
GBL0101 <reor97268@gmail.com>
2024-06-05 16:47:24 +0900

    curriculum8-4

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/', [PostController::class, 'index']);
 
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::post('/posts', [PostController::class, 'store']);
+
 Route::get('/posts/{post}', [PostController::class ,'show']);
